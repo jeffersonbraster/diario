07d8150b12fb14efc039f8e691bfb92c5a999de5
Jefferson Brandão <jeffersonbraster@gmail.com>
2020-09-06 15:33:00 -0300

    auth routes

---
+++ routes/auth.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const passport = require('passport')
+const router = express.Router()
+
+//@desc Auth com google
+//@route get auth/google
+router.get('/google', passport.authenticate('google', {scope: ['profile']}))
+
+//@desc Google autenticação callback
+//@route get /auth/google/callback
+router.get('/google/callback', passport.authenticate('google', {failureRedirect: '/'}), (req, res) => {
+    res.redirect('/dashboard')
+})
+
+module.exports = router
