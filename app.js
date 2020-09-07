cf1df532e19d1d649fb74b148b91029c1f2d6e40
Jefferson Brandão <jeffersonbraster@gmail.com>
2020-09-06 22:42:19 -0300

    method override para PUT request

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const morgan = require('morgan')
 const passport = require('passport');
 const session = require('express-session')
@@ -22,6 +23,16 @@ const app = express()
 //analisar body
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+
+//metodo override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  }))
 
 //Logs
 if(process.env.NODE_ENV === 'development') {
