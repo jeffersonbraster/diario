1003b443915cc7e870880eeb930ec9eb93753f31
Jefferson Brandão <jeffersonbraster@gmail.com>
2020-09-06 22:30:10 -0300

    Editando historias

--- routes/stories.js
+++ routes/stories.js
@@ -41,6 +41,32 @@ router.get('/', ensureAuth, async(req, res) => {
     }
 })
 
+//@desc Mostrar edit page
+//@route get /stories/edit/:id
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+    try {
+        const story = await Story.findOne({
+          _id: req.params.id,
+        }).lean()
+    
+        if (!story) {
+          return res.render('error/404')
+        }
+    
+        if (story.user != req.user.id) {
+          res.redirect('/stories')
+        } else {
+          res.render('stories/edit', {
+            story,
+          })
+        }
+      } catch (err) {
+        console.error(err)
+        return res.render('error/500')
+      }
+    
+})
+
     
 
 module.exports = router
