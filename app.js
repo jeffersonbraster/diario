cf2a0e1d03b4dfcebd20c7e6f29b77adfc100230
Jefferson Brandão <jeffersonbraster@gmail.com>
2020-09-06 22:07:28 -0300

    StrigTags e truncate em historias

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ if(process.env.NODE_ENV === 'development') {
 }
 
 //Handlebars Helper
-const {formatDate} = require('./helpers/hbs')
+const {formatDate, stripTags, truncate} = require('./helpers/hbs')
 
 //Handlebars
-app.engine('.hbs', exphbs({helpers: {formatDate}, defaultLayout: 'main', extname: '.hbs'}))
+app.engine('.hbs', exphbs({helpers: {formatDate, stripTags, truncate}, defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', '.hbs')
 
 //Sessions
