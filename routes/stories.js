5dced3b267128f059fc2d6bad59fe3a2c92e6aa5
Jefferson Brandão <jeffersonbraster@gmail.com>
2020-09-06 23:09:04 -0300

    user stories

--- routes/stories.js
+++ routes/stories.js
@@ -118,6 +118,26 @@ router.delete('/:id', ensureAuth, async(req, res) => {
     console.error(err)
     return res.render('error/500')
   }
-})  
+})
+
+//@desc user historias
+//@route get /stories/user/:userid
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({
+      user: req.params.userId,
+      status: 'public'
+    })
+    .populate('user')
+    .lean()
+    
+    res.render('stories/index', {
+      stories
+    })
+  } catch (error) {
+    console.error(error)
+    res.render('error/500')
+  }
+})
 
 module.exports = router
